f04d86ea5e2dc9b01cff5255f46bad080f011726
Phạm Văn Sơn <vanson9x.it@gmail.com>
2018-10-01 10:11:01 +0700

    no message

---
+++ CS/CS07-08.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CS07-08.aspx.cs" Inherits="CS.CS07" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Yeu cau 1</h1>
+    <form action="CS07" method="post" onsubmit="return funcSubmit()">
+        Ho Ten: <input id="inHoTen" required /> <br />
+        Nam Sinh: <input type="date" id="inBirthday" required /> <br />
+        Email: <input type="email" id="inEmail" required /> <br />
+        <button type="submit">Submit</button>
+    </form>
+    <h1>Yeu cau 2</h1>
+    <form id="form1" runat="server">
+        <div>
+            Ho Ten: <asp:TextBox ID="txtHoTen" runat="server"></asp:TextBox> 
+            <asp:CustomValidator ID="CustomValidator1" runat="server" 
+                ErrorMessage="Ho va Ten khong hop le" 
+                ValidateEmptyText="true"
+                ForeColor ="RED"
+                ControlToValidate ="txtHoTen" OnServerValidate="CustomValidator1_ServerValidate"></asp:CustomValidator><br />
+            Nam Sinh: <asp:TextBox ID="txtNamSinh" TextMode="Date" runat="server"></asp:TextBox>
+            <asp:CustomValidator ID="CustomValidator2" runat="server"
+                 ErrorMessage="Nam sinh khong hop le" 
+                ValidateEmptyText="true" 
+                ForeColor ="RED"
+                ControlToValidate ="txtNamSinh" OnServerValidate="CustomValidator2_ServerValidate"></asp:CustomValidator> <br />
+            Email: <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <asp:CustomValidator ID="CustomValidator3" runat="server"
+                 ErrorMessage="Email khong hop le" 
+                ValidateEmptyText="true" 
+                ForeColor ="RED"
+                ControlToValidate ="txtEmail" OnServerValidate="CustomValidator3_ServerValidate" ></asp:CustomValidator> <br />
+            <asp:Button ID="btnSend" Text="Submit" runat="server" OnClick="btnSend_Click" />
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ CS/CS07-08.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS
+{
+    public partial class CS07 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if(args.Value == "")
+            {
+                CustomValidator1.ErrorMessage = "Không được để trống";
+                args.IsValid = false;
+            }
+        }
+
+        protected void CustomValidator2_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            DateTime dt;
+            if (args.Value == "")
+            {
+                CustomValidator1.ErrorMessage = "Không được để trống";
+                args.IsValid = false;
+            } else if(DateTime.TryParse(txtNamSinh.Text, out dt))
+            {
+                if(DateTime.Now.Year - dt.Year <= 16 || DateTime.Now.Year - dt.Year > 50)
+                {
+                    CustomValidator1.ErrorMessage = "Năm sinh không hợp lệ. Tuổi từ 16 đến 50";
+                    args.IsValid = false;
+                }
+            }
+        }
+
+        protected void CustomValidator3_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (args.Value == "")
+            {
+                CustomValidator1.ErrorMessage = "Không được để trống";
+                args.IsValid = false;
+            } else
+            {
+                try
+                {
+                    new System.Net.Mail.MailAddress(txtEmail.Text);
+                    if(txtEmail.Text.ToString().ToLower().Contains("@yahoo.com.vn"))
+                    {
+                        CustomValidator1.ErrorMessage = "Khong duoc su dung @yahoo.com.vn";
+                        args.IsValid = false;
+                    } else
+                    {
+                        using(SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                        {
+                            using (SqlCommand cmd = new SqlCommand("SELECT sEmail FROM tblUsers WHERE eEmail = '" + txtEmail.Text + "'", conn))
+                            {
+                                conn.Open();
+                                SqlDataReader sdr = cmd.ExecuteReader();
+                                Boolean exists = false;
+                                while (sdr.Read())
+                                {
+                                    if(sdr.GetString(0).Equals(txtEmail.Text))
+                                    {
+                                        exists = true;
+                                        break;
+                                    }
+                                }
+                                if(exists)
+                                {
+                                    CustomValidator1.ErrorMessage = "Email da ton tai";
+                                    args.IsValid = false;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    CustomValidator1.ErrorMessage = "Email khong hop le";
+                    args.IsValid = false;
+                }
+            }
+        }
+    }
+}
